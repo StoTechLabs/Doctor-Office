0b602b7f4d11b51176bd88f30e40c773e6ef0aa5
Joshua Storey & Erica Gordon <student@epicodus.com>
2014-08-13 10:30:53 -0700

    Adds spec to return name. Spec passes.

--- spec/doctor_spec.rb
+++ spec/doctor_spec.rb
@@ -3,10 +3,13 @@ require 'rspec'
 
 describe Doctor do
 
-    it 'is initialized with a name and specialty id' do
+  it 'is initialized with a name and specialty id' do
     test_doctor = Doctor.new({:name =>'Brown', :specialty_id => 2})
     expect(test_doctor).to be_an_instance_of Doctor
   end
 
-
+  it 'returns the name' do
+    test_doctor = Doctor.new({:name => 'Brown', :specialty_id => 2})
+    expect(test_doctor.name). to eq 'Brown'
+  end
 end
