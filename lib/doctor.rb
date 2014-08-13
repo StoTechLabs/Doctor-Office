7e7464ad44275c8d575d5d160dded43eec845051
Joshua Storey & Erica Gordon <student@epicodus.com>
2014-08-13 10:23:48 -0700

    Add attr_reader for defined instance variables, name and specialty.

--- lib/doctor.rb
+++ lib/doctor.rb
@@ -1,5 +1,9 @@
 class Doctor
+  attr_reader :name, :specialty_id
+
   def initialize(attributes)
+    @name = attributes[:name]
+    @specialty_id = attributes[:specialty_id]
   end
 
 end
---
+++ spec/patient_spec.rb
@@ -0,0 +1,2 @@
+require 'patient'
+require 'rspec'
