b38bfe6b359f692185048c74e1cbd4da2463282d
Joshua Storey & Erica Gordon <student@epicodus.com>
2014-08-13 10:19:34 -0700

    Create spec for instance of Doctor class. Spec passes.

---
+++ spec/doctor_spec.rb
@@ -0,0 +1,11 @@
+require 'doctor'
+require 'rspec'
+
+describe Doctor do
+  it 'is initialized with a name and specialty id' do
+  test_doctor = Doctor.new({:name =>'Brown', :specialty_id => 2})
+  expect(test_doctor).to be_an_instance_of Doctor
+end
+
+
+end
