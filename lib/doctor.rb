19d58e47c88eff0dd38672c9de557621eae3639b
Joshua Storey & Erica Gordon <student@epicodus.com>
2014-08-13 11:29:12 -0700

    Create functionality to save doctor info to database. Spec passes.

--- lib/doctor.rb
+++ lib/doctor.rb
@@ -6,9 +6,18 @@ class Doctor
     @specialty_id = attributes[:specialty_id]
   end
 
+  def save
+   results =DB.exec("INSERT INTO doctor (name, specialty_id) VALUES ('#{name}', #{specialty_id});")
+  end
+
   def self.all
     results = DB.exec("SELECT * FROM doctor;")
     doctors = []
+    results.each do |result|
+      name = result['name']
+      specialty_id = result['specialty_id'].to_i
+      doctors << Doctor.new({:name => name, :specialty_id => specialty_id})
+    end
     doctors
   end
 
--- spec/doctor_spec.rb
+++ spec/doctor_spec.rb
@@ -20,11 +20,17 @@ describe Doctor do
 
   it 'returns the name' do
     test_doctor = Doctor.new({:name => 'Brown', :specialty_id => 2})
-    expect(test_doctor.name). to eq 'Brown'
+    expect(test_doctor.name).to eq 'Brown'
   end
 
   it 'starts with an empty array of Doctors' do
     expect(Doctor.all).to eq []
+  end
+
+  it 'saves a doctor to the database' do
+    test_doctor = Doctor.new(:name => 'Brown', :specialty_id => 2)
+    test_doctor.save
+    expect(Doctor.all).to eq [test_doctor]
   end
 
 
