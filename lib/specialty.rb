4982153e5a2b58f3ecacbfe8caab52b0c7c7bbb0
Joshua Storey & Erica Gordon <student@epicodus.com>
2014-08-14 11:22:44 -0700

    Create functionality to save specialty id and return the id.

--- lib/specialty.rb
+++ lib/specialty.rb
@@ -1,6 +1,26 @@
+
+
 class Specialty
+ attr_reader :name, :id
+
   def initialize(attributes)
-    @name = attributes['name']
+    @name = attributes[:name]
+    @id = attributes[:id]
+  end
+
+  def save
+  results = DB.exec("INSERT INTO Specialty (name) VALUES ('#{name}') RETURNING id;")
+  @id = results.first['id'].to_i
+  end
+
+  def self.all
+    results = DB.exec("SELECT * FROM specialty;")
+    specialty = []
+    results.each do |result|
+      name = result['name']
+      specialty << Specialty.new({:name => name})
+    end
+    specialty
   end
 
 end
--- spec/specialty_spec.rb
+++ spec/specialty_spec.rb
@@ -7,5 +7,11 @@ describe Specialty do
     expect(test_specialty).to be_an_instance_of Specialty
   end
 
+  it 'sets the specialty id when it is saved' do
+    test_specialty = Specialty.new({:name => 'Hand'})
+    test_specialty.save
+    expect(test_specialty.id).to be_an_instance_of Fixnum
+  end
+
 
 end
